0730c27a0d560c9f7eb37746d26d0e8b2bec4122
Hi10San <prajapati.hiten05@gmail.com>
2026-07-06 17:00:59 +0530

    fixed blog text

--- frontend/src/components/ui/TextPressure.tsx
+++ frontend/src/components/ui/TextPressure.tsx
@@ -44,7 +44,7 @@ const debounce = (func: (...args: any[]) => void, delay: number) => {
 const TextPressure: React.FC<TextPressureProps> = ({
   text = 'Compressa',
   fontFamily = 'Compressa VF',
-  fontUrl = 'https://res.cloudinary.com/dr6lvwubh/raw/upload/v1529908256/CompressaPRO-GX.woff2',
+  fontUrl = '/fonts/CompressaPRO-GX.woff2',
   width = true,
   weight = true,
   italic = true,
